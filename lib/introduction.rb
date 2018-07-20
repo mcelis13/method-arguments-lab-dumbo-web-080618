18fd23cd6c814430cb7af26ccb888466c6391443
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 11:34:46 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 def introduction(name)
-  puts `My name is, #{name}.\n`
+  puts `Hi, name is #{name}.\n`
 end
 
 def introduction_with_language(name, language)
-  puts `My name is #{name}, and I'm learning to program in #{language}.\n`
+  puts `Hi, my name is #{name} and I'm learning to program in #{language}.\n`
 end
